3d5e71c563c4c73e4502b725d502d7f7c320ffa6
Valerij Zimnev <sorption.music@gmail.com>
2018-08-04 20:09:32 +0700

    SAVE

--- app.js
+++ app.js
@@ -1,20 +1,57 @@
 const PATH = require('path');
 const dirTree = require('directory-tree');
 const fs = require('fs');
+const copydir = require('copy-dir');
 
-const LayoutHeader = require('./app/header');
-const LayoutFooter = require('./app/footer');
+const LayoutHeader = require('./src/header');
+const LayoutFooter = require('./src/footer');
 
-const tree = dirTree('./src', {extensions:/\.txt$/}, (item, PATH) => {
-	// console.log(item);
-});
+const tree = dirTree(
+  './images',
+  { extensions: /\.(jpeg|jpg|png|svg|gif)$/ },
+  (item, PATH) => {
+    // console.log(item);
+  },
+);
 
-const stream = fs.createWriteStream("build/my_file.html");
+let template = '';
+
+function doTemplate(inputArray) {
+  
+  inputArray.forEach(element => {
+    if (element['type'] !== 'directory') {
+      template += `<img src="${element['path']}" width="100" />\n`;
+    } else {
+      // console.log('DIRECTORY ' + element['name']);
+      template += `<div class="inside">\n`;
+      template += `<h2>${element['name']}</h2>\n`;
+      doTemplate(element['children']);
+      template += `</div>\n`;
+    }
+  });
+
+  return template;
+}
+
+// console.log(doTemplate(tree['children']));
+
+const stream = fs.createWriteStream('build/rendered.html');
 stream.once('open', function(fd) {
+
+  const imagesList = tree['children'].sort(function(a, b) {
+    const keyA = new Date(b.type),
+          keyB = new Date(a.type);
+    // Compare the 2 dates
+    if (keyA < keyB) return -1;
+    if (keyA > keyB) return 1;
+    return 0;
+  });
+
   stream.write(LayoutHeader('Первый раз', 'lolka'));
-  stream.write(JSON.stringify(tree));
-  stream.write(LayoutFooter())
+  stream.write(doTemplate(imagesList));
+  stream.write(LayoutFooter());
   stream.end();
 });
 
+copydir.sync('images', 'build/images');
 // console.log(tree);
