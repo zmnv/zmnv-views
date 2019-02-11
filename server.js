9108c8ce27c8119aa4c9640501fa6803ff8d065e
Valerij Zimnev <sorption.music@gmail.com>
2019-02-11 19:07:13 +0700

    Серверок

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
 const opn = require('opn');
+const chalk = require('chalk').default;
+const {LayoutLine} = require('./src/js-helpers/consoleTools');
+
 const address = require('./src/js-helpers/getMyAddress');
+const hostname = `http://${address.ip}:${address.port}`;
+
 
 app.use('/', express.static(__dirname + '/build')); // ← adjust
 
 app.listen(address.port, address.ip, function() { 
     console.log(`\x1b[90m
-┌─────────────────────────────────────────────┐
-│ \x1b[95mRunning Server \x1b[90m* \x1b[94mhttp://${address.ip}:${address.port}\x1b[90m  │
-└─────────────────────────────────────────────┘
+${chalk.gray('┌──────────────────────────────────────────────┐')}
+${chalk.gray('│')} ${chalk.magentaBright('Running Server')} ${chalk.gray('*')} ${LayoutLine(hostname, 27, undefined, chalk.cyanBright)} ${chalk.gray('│')}
+${chalk.gray('└──────────────────────────────────────────────┘')}
 Press CTRL+C to exit...
     \x1b[0m`);
     opn(`http://${address.ip}:${address.port}`);
